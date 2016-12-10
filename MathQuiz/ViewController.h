d84da0240d745129a1a281b7c160d4cee9a58b95
Nirupama Prasad <nirupamaprasad@nirupamas-macbook-air.local>
2016-12-09 21:31:19 -0800

    Initial Commit

---
+++ MathQuiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MathQuiz
+//
+//  Created by Nirupama Prasad on 12/9/16.
+//  Copyright © 2016 Nirupama Prasad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MathQuiz/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MathQuiz
+//
+//  Created by Nirupama Prasad on 12/9/16.
+//  Copyright © 2016 Nirupama Prasad. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
